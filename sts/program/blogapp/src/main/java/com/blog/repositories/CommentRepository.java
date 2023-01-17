8c3260e4b2d60369a64f32750877b380690e31c0
Suresh Sencha <115613661+senchasuresh99@users.noreply.github.com>
2023-01-17 14:30:33 +0530

    Delete sts/program/blogapp directory

--- sts/program/blogapp/src/main/java/com/blog/repositories/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blog.entities.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
